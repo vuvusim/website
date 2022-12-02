17509de691de15544f7ba2f652e323909dd29db8
simas venskus <simasvenskus@gmail.com>
2022-12-02 11:27:21 +0200

    Projects and single project templates (TRY)

--- website/projects/views.py
+++ website/projects/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from .models import Project
 
+projectsList = [
+    {
+        'id':'1',
+        'title':'Gym Website',
+        'description':'Fully functional Gym website',
+    },
+    {
+        'id':'2',
+        'title':'Recepie Website',
+        'description':'Fully functional Recepie website',
+    },
+    {
+        'id':'3',
+        'title':'Bike repair',
+        'description':'Fully functional Bike repair website'
+    },
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'Projects'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i ['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})

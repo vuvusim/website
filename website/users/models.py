9c1407fe87d0dd0d83ed78c3d8f92a3d3a1a128c
simas venskus <simasvenskus@gmail.com>
2022-12-05 19:40:53 +0200

    profile page, skill DB,

--- website/users/models.py
+++ website/users/models.py
@@ -7,14 +7,23 @@ class Profile(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=500, null=True, blank=True)
     username = models.CharField(max_length=500, null=True, blank=True)
+    location = models.CharField(max_length=500, null=True, blank=True)
     short_intro = models.CharField(max_length=500, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     profile_image = models.ImageField(null=True, blank=True, default='user-default.png')
-    social_twitter = models.CharField(max_length=200, null=True, blank=True)
-    social_instagram = models.CharField(max_length=200, null=True, blank=True)
-    social_facebook = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)

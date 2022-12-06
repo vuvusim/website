791edba7683f8e6e5a84a830c671d3d9e9994442
simas venskus <simasvenskus@gmail.com>
2022-12-06 10:42:16 +0200

    user creation form and conditions With messages

--- website/users/views.py
+++ website/users/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from .models import Profile
+from .forms import CustomUserCreationForm
 
+#-- Login user authentication --#
 def loginUser(request):
+    page = 'login'
+
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -16,6 +21,7 @@ def loginUser(request):
         except:
             messages.error(request, 'Username does not exist')
         user = authenticate(request, username=username, password=password)
+        #-- if user exists send to profiles --#
         if user is not None:
             login(request, user)
             return redirect('profiles')
@@ -24,10 +30,32 @@ def loginUser(request):
 
     return render(request, 'users/login_register.html')
 
+#-- logout user send to login --#
 def logoutUser(request):
     logout(request)
     messages.error(request, 'User was loged out')
     return redirect('login')
+
+#-- register user (same template) --#
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    #-- condition for user only lowercase --#
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'User account was created!')
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'An error has accured during registration')
+            
+    context= {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
@@ -36,8 +64,10 @@ def profiles(request):
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-
+    
+    #-- skill with description --#
     topSkills = profile.skill_set.exclude(description__exact="")
+    #-- skill withouth description --#
     otherSkills = profile.skill_set.filter(description="")
 
     context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}

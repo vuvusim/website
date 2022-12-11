9269d9f5884aa348550ba2abad7c989d4f2d6489
simas venskus <simasvenskus@gmail.com>
2022-12-11 20:13:47 +0200

    sending emails

--- website/website/settings.py
+++ website/website/settings.py
@@ -130,3 +130,11 @@ MEDIA_ROOT = BASE_DIR.joinpath('media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.eu'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'simasvenskus@zohomail.eu'
+EMAIL_HOST_PASSWORD = 'Dm0xDDqdhg5Y'
--- website/website/urls.py
+++ website/website/urls.py
@@ -22,7 +22,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
+
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+# 1 - User submits email for reset              //PasswordResetView.as_view()           //name="reset_password"
+# 2 - Email sent message                        //PasswordResetDoneView.as_view()        //name="passsword_reset_done"
+# 3 - Email with link and reset instructions    //PasswordResetConfirmView()            //name="password_reset_confirm"
+# 4 - Password successfully reset message       //PasswordResetCompleteView.as_view()   //name="password_reset_complete"
